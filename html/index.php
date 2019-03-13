1d27097a7897dcde468c495f17c21cc6e848c579
m.lutin <mickael.lutin@fluedis.com>
2019-03-13 17:21:20 +0100

    mise en place de phpmyadmin

--- html/index.php
+++ html/index.php
@@ -6,17 +6,26 @@
  * Time: 11:39
  */
 
-$name = 'Hello Mickael';
-echo "test: ".$name.'<br>';
+$name = 'Hello Mickael: ';
 
 $username = "mickael";
 $password = "root";
 
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=sit-vanilla", $username, $password);
-    echo "Connected successfully ".$name;
-}
-catch(PDOException $e)
-{
+    $pdo = new PDO('mysql:host=mysql;dbname=sit-vanilla;charset=utf8', $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
     echo $name." Connection failed: " . $e->getMessage();
+}
+
+  $pdo->exec('CREATE table IF NOT EXISTS test1(
+     ID INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
+     firstName VARCHAR( 250 ) NOT NULL);');
+
+  $pdo->query('INSERT INTO test1 values (NULL , "john")');
+
+  $sql = 'SELECT * FROM test1';
+
+  foreach ($pdo->query($sql)->fetch() as $name) {
+    echo $name.'<br>';
 }
--- phpmyadmin/tbl_sql.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "phpmy admin";
